a06d08b22b6f9b66788398b28911d006d71459fa
Yahya <yahyatinani@gmail.com>
2020-08-13 20:22:41 +0100

    Implement readPeopleFromFile function

--- src/Main.kt
+++ src/Main.kt
@@ -11,8 +11,8 @@ fun person(input: Input): Result<Pair<Person, Input>> =
                         fName.second.readString("Enter your last name:")
                                 .map { lName ->
                                     Pair(Person(id.first,
-                                                fName.first,
-                                                lName.first), lName.second)
+                                            fName.first,
+                                            lName.first), lName.second)
                                 }
                     }
         }
@@ -21,6 +21,18 @@ fun person(input: Input): Result<Pair<Person, Input>> =
 private fun readPeopleFromConsole(): List<Person> =
         Stream.unfold(ConsoleReader(), ::person).toList()
 
+private fun readPeopleFromFile(filename: String): Result<List<Person>> =
+        FileReader(filename).map { fileReader ->
+            fileReader.use {
+                Stream.unfold(it, ::person).toList()
+            }
+        }
+
 fun main() {
-    readPeopleFromConsole().forEach(::println)
+//    readPeopleFromConsole().forEach(::println)
+
+    readPeopleFromFile("ppl.txt")
+            .forEach({ list ->
+                list.forEach(::println)
+            }, onFailure = ::println)
 }
