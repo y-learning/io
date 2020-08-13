5ac787ed131a20a68556d85e2cf19a197da8ff5b
Yahya <yahyatinani@gmail.com>
2020-08-13 19:17:24 +0100

    Implement Person reader from console

--- src/Main.kt
+++ src/Main.kt
@@ -1,17 +1,26 @@
+import list.List
+import result.Result
+import stream.Stream
+
+data class Person(val id: Int, val firstName: String, val lastName: String)
+
+fun person(input: Input): Result<Pair<Person, Input>> =
+        input.readInt("Enter your ID:").flatMap { id ->
+            id.second.readString("Enter your first name:")
+                    .flatMap { fName ->
+                        fName.second.readString("Enter your last name:")
+                                .map { lName ->
+                                    Pair(Person(id.first,
+                                                fName.first,
+                                                lName.first), lName.second)
+                                }
+                    }
+        }
+
+
 fun main() {
-    val input = ConsoleReader()
-
-    val rStr = input.readString("Enter your name:").map { it.first }
-
-    val greetingName = rStr.map { "Hello, $it" }
-
-    greetingName.forEach(::println, onFailure = { println(it.message) })
-
-    val rInt = input.readInt("Enter your age:").map { it.first }
-
-    val commentAge = rInt.map { "You look younger than $it : )" }
-
-    commentAge.forEach(::println, onFailure = {
-        println("Invalid age. Please enter an integer.")
-    })
+    readPeopleFromConsole().forEach(::println)
 }
+
+private fun readPeopleFromConsole(): List<Person> =
+        Stream.unfold(ConsoleReader(), ::person).toList()
